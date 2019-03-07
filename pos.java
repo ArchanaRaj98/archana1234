4244db5a7214fa78200b6b695c465e62320f4d42
ArchanaRaj98 <47383624+ArchanaRaj98@users.noreply.github.com>
2019-03-07 07:55:24 -0800

    Create pos.java

---
+++ pos.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class posnegzer
+{
+    public static void main(String[]args)
+    
+    {
+    int i=3;
+    if(i>0)
+    System.out.println("Positive");
+    else if(i<0)
+    System.out.println("Negative");
+    else
+    System.out.println("Zero");
+    
+    }
+}
